f421939bf8deb56eff511a4668ddb7399489f099
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2018-01-27 23:39:28 +0200

    Added empty demo project

---
+++ Demo/CellViewModelDemo/CellViewModelDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CellViewModelDemo
+//
+//  Created by Anton Poltoratskyi on 27.01.18.
+//  Copyright © 2018 Anton Poltoratskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
