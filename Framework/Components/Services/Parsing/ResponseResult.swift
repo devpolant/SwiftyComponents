a69504ba5028ddd189465105e7cff8cab939d43d
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2018-01-29 01:01:15 +0200

    Processed arrays

--- Framework/Components/Services/Parsing/ResponseResult.swift
+++ Framework/Components/Services/Parsing/ResponseResult.swift
@@ -15,6 +15,8 @@ enum ResponseResult<T> {
 
 // MARK: - Parsing
 
+// MARK: Single Entity
+
 extension ResponseResult where T: WebResponseProtocol {
     
     typealias ResultType = T.ResponseData
@@ -24,6 +26,28 @@ extension ResponseResult where T: WebResponseProtocol {
         case let .success(response):
             if response.status, let data = response.data {
                 return .success(data)
+            }
+            return processing(errorMessage: response.error)
+        case let .failure(error):
+            return processing(error: error)
+        }
+    }
+}
+
+// MARK: Collections
+
+protocol JsonArray: RandomAccessCollection { }
+extension Array: JsonArray { }
+
+extension ResponseResult where T: WebResponseProtocol, T.ResponseData: JsonArray {
+    
+    typealias ResultCollection = [T.ResponseData.Element]
+    
+    func processCollection() -> ResponseResult<ResultCollection> {
+        switch self {
+        case let .success(response):
+            if response.status, let data = response.data {
+                return .success(data.map { $0 })
             }
             return processing(errorMessage: response.error)
         case let .failure(error):
