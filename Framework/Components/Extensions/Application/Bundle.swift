4fece0030f27216f061d9035a40837bb6fbe00ce
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2018-01-30 22:50:11 +0200

    Added Bundle extensions

---
+++ Framework/Components/Extensions/Application/Bundle.swift
@@ -0,0 +1,25 @@
+//
+//  Bundle.swift
+//  frameworx
+//
+//  Created by Anton Poltoratskyi on 10/31/17.
+//  Copyright © 2017 Appus Studio LP. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+    public var bundleIdentifier: String {
+        return object(forInfoDictionaryKey: kCFBundleIdentifierKey as String) as! String
+    }
+    public var bundleName: String {
+        return object(forInfoDictionaryKey: kCFBundleNameKey as String) as! String
+    }
+    public var displayName: String {
+        let displayName = object(forInfoDictionaryKey: "CFBundleDisplayName") as? String
+        return displayName ?? self.bundleName
+    }
+    public var buildVersion: String? {
+        return object(forInfoDictionaryKey: kCFBundleVersionKey as String) as? String
+    }
+}
