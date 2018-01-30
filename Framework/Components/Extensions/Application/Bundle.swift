88f1be452bba2ff2f610b5928adcf4ce80840065
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2018-01-30 22:53:43 +0200

    Add comments

--- Framework/Components/Extensions/Application/Bundle.swift
+++ Framework/Components/Extensions/Application/Bundle.swift
@@ -12,13 +12,18 @@ extension Bundle {
     public var bundleIdentifier: String {
         return object(forInfoDictionaryKey: kCFBundleIdentifierKey as String) as! String
     }
+    
+    /// Project bundle name
     public var bundleName: String {
         return object(forInfoDictionaryKey: kCFBundleNameKey as String) as! String
     }
+    
+    /// App name which displaying in Springboard
     public var displayName: String {
         let displayName = object(forInfoDictionaryKey: "CFBundleDisplayName") as? String
         return displayName ?? self.bundleName
     }
+    
     public var buildVersion: String? {
         return object(forInfoDictionaryKey: kCFBundleVersionKey as String) as? String
     }
